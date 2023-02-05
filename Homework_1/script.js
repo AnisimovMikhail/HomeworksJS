e41551193bfd1082b276c8d3095a74ec5bbd583b
Mikhail <misha.anisimov.2012@mail.ru>
2023-02-05 20:25:08 +0400

    Homework_1

---
+++ Homework_1/script.js
@@ -0,0 +1,13 @@
+// Задание 1
+
+// let Celsius = 32;
+// Fahrenheit = (9 / 5);
+// console.log(`Градусы Цельсия: ${Celsius}
+// Градусы Фаренгейта: ${Fahrenheit * Celsius + 32}`);
+
+
+// Задание 2
+
+// let name = 'Михаил';
+// admin = 'Михаил';
+// console.log(admin);
