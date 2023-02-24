3efab383f7858c79bdedec1feca29c18dd467c75
Mikhail <misha.anisimov.2012@mail.ru>
2023-02-24 17:23:36 +0400

    HomeWork_10

---
+++ Homework_10/data.js
@@ -0,0 +1,30 @@
+const data = `[
+    {
+        "id" : "1",
+        "activity": "Play a game of tennis with a friend",
+        "type": "social",
+        "participants": 2,
+        "img" : "https://tennis77.com/wp-content/uploads/2018/12/st72.png"
+      },
+      {
+        "id" : "2",
+        "activity": "Mow your neighbor's lawn",
+        "type": "charity",
+        "participants": 1,
+        "img" : "https://cdn.fishki.net/upload/post/2019/07/21/3037872/gallery/007.jpg"
+      },
+      {
+        "id" : "3",
+        "activity": "Watch a classic movie",
+        "type": "recreational",
+        "participants": 1,
+        "img" : "https://www.kinonews.ru/insimgs/2018/shotimg/shotimg80532_7.jpg"
+      },
+      {
+        "id" : "4",
+        "activity": "Learn Javascript",
+        "type": "education",
+        "participants": 1,
+        "img" : "https://handsontable.com/blog/wp-content/uploads/2018/07/best-resources-to-learn-javascript.png"
+      }
+]`
